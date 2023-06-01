0d815fa04b17e375a5c3669bc60315b7fb299ce2
swoga <swoga@users.noreply.github.com>
2023-06-01 22:47:58 +0200

    feat: ignore NOP state changes

--- components/balboa/switch.cpp
+++ components/balboa/switch.cpp
@@ -5,7 +5,13 @@ namespace balboa {
 
 void BalboaSwitch::set_parent(BalboaComponent *parent) { this->parent = parent; }
 
-void BalboaSwitch::write_state(bool state) { parent->send_toggle_item(item); }
+void BalboaSwitch::write_state(bool state) {
+  if (this->state == state) {
+    ESP_LOGI(TAG, "ignore nop state change");
+    return;
+  }
+  parent->send_toggle_item(item);
+}
 
 }  // namespace balboa
 }  // namespace esphome
